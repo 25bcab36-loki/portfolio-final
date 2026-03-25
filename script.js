f98181c453fdbf182826357df3c2f8b7e7521ae9
25bcab36-loki <25bcab36@kristujayanti.com>
2026-03-25 09:29:07 +0530

    Initial commit

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch("https://portfolio-final-1-683p.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
